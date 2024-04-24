6450abd06ec734978bbf684c4d1167488ef659b0
Bradley Love <brad@bradlove.co.za>
2024-04-24 11:27:22 +0200

    Update index.ts

--- services/account/src/index.ts
+++ services/account/src/index.ts
@@ -29,8 +29,8 @@ export const start = async () => {
     });
 
     try {
-        await server.listen({ host: env.API_HOST, port: env.API_PORT });
-        Logger.info("Start", `Server listening at ${env.API_PORT}`);
+        await server.listen({ host: env.ACCOUNT_HOST, port: env.ACCOUNT_PORT });
+        Logger.info("Start", `Server listening at ${env.ACCOUNT_PORT}`);
     } catch (error) {
         if (error instanceof Error) {
             Logger.error("Start", error.message);

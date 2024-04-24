31c4783d372b2d08141f32958345d6a46d77af7d
Bradley Love <brad@bradlove.co.za>
2024-04-24 14:03:55 +0200

    Update index.ts

--- packages/environment/index.ts
+++ packages/environment/index.ts
@@ -11,8 +11,8 @@ const analyticsEnvSchema = z.object({
 const accountEnvSchema = z.object({
   ACCOUNT_PORT: z.coerce.number().default(8081),
   ACCOUNT_HOST: z.string().default("127.0.0.1"),
-  SUPBASE_URL: z.string().default("url"),
-  SUPBASE_KEY: z.string().default("url"),
+  SUPABASE_URL: z.string().default("url"),
+  SUPABASE_KEY: z.string().default("url"),
 });
 
 const envSchema = z.union([

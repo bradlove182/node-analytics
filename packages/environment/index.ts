06f7386d947f2c0e05492f7dbb65ada3a518396e
Bradley Love <brad@bradlove.co.za>
2024-04-26 15:48:58 +0200

    Added tailwind to dashboard

--- packages/environment/index.ts
+++ packages/environment/index.ts
@@ -1,18 +1,18 @@
 import { z } from "zod";
 
 const analyticsEnvSchema = z.object({
-  CLICKHOUSE_DATABASE_URL: z.string().default("url"),
-  CLICKHOUSE_DATABASE_USERNAME: z.string().default("default"),
-  CLICKHOUSE_DATABASE_PASSWORD: z.string().default("password"),
-  ANALYTICS_PORT: z.coerce.number().default(8080),
-  ANALYTICS_HOST: z.string().default("127.0.0.1"),
+    CLICKHOUSE_DATABASE_URL: z.string().default("url"),
+    CLICKHOUSE_DATABASE_USERNAME: z.string().default("default"),
+    CLICKHOUSE_DATABASE_PASSWORD: z.string().default("password"),
+    ANALYTICS_PORT: z.coerce.number().default(8080),
+    ANALYTICS_HOST: z.string().default("127.0.0.1"),
 });
 
 const accountEnvSchema = z.object({
-  ACCOUNT_PORT: z.coerce.number().default(8081),
-  ACCOUNT_HOST: z.string().default("127.0.0.1"),
-  SUPABASE_URL: z.string().default("url"),
-  SUPABASE_KEY: z.string().default("url"),
+    ACCOUNT_PORT: z.coerce.number().default(8081),
+    ACCOUNT_HOST: z.string().default("127.0.0.1"),
+    SUPABASE_URL: z.string().default("url"),
+    SUPABASE_KEY: z.string().default("url"),
 });
 
 const envSchema = analyticsEnvSchema.merge(accountEnvSchema);
--- services/account/src/client/index.ts
+++ services/account/src/client/index.ts
@@ -1,4 +1,4 @@
-import { env } from '@repo/environment';
-import { createClient } from '@supabase/supabase-js';
+import { env } from "@repo/environment";
+import { createClient } from "@supabase/supabase-js";
 
 export const client = createClient(env.SUPABASE_URL, env.SUPABASE_KEY);

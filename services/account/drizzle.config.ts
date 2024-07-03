5aba607831c4b07c649f3f94f45253ccac467f2d
Bradley Love <brad@bradlove.co.za>
2024-07-03 09:25:04 +0200

    upgrade turbo + drizzle

--- services/account/drizzle.config.ts
+++ services/account/drizzle.config.ts
@@ -1,10 +1,10 @@
 import { env } from "@repo/environment";
-import type { Config } from "drizzle-kit";
-export default {
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
     schema: "./src/database/schemas.ts",
     out: "./src/database/migrations",
     dialect: "postgresql",
     dbCredentials: {
         url: env.ACCOUNT_DATABASE_URL,
     },
-} satisfies Config;
+});

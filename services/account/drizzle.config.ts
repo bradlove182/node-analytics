6fe186cbb100075ea5e9256b0ec48488b2ac97a3
Bradley Love <brad@bradlove.co.za>
2024-07-03 09:07:40 +0200

    Update packages

--- services/account/drizzle.config.ts
+++ services/account/drizzle.config.ts
@@ -1,6 +1,10 @@
+import { env } from "@repo/environment";
 import type { Config } from "drizzle-kit";
 export default {
     schema: "./src/database/schemas.ts",
     out: "./src/database/migrations",
-    driver: "pg", // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
+    dialect: "postgresql",
+    dbCredentials: {
+        url: env.ACCOUNT_DATABASE_URL,
+    },
 } satisfies Config;

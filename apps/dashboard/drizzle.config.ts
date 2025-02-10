29c6eb0ae3e6b6d39284d38688a6f4bc626ea8b5
Bradley Love <brad@bradlove.co.za>
2025-02-10 10:13:46 +0200

    Tailwind v4 themeing

--- apps/dashboard/drizzle.config.ts
+++ apps/dashboard/drizzle.config.ts
@@ -6,6 +6,7 @@ if (!process.env.DATABASE_URL)
 
 export default defineConfig({
     schema: "./src/lib/server/database/schemas/index.ts",
+    out: "./src/lib/server/database/migrations",
     dbCredentials: {
         url: process.env.DATABASE_URL,
     },
--- apps/dashboard/src/routes/login/+page.svelte
+++ apps/dashboard/src/routes/login/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
    import { enhance } from "$app/forms";
+    import Button from "$lib/client/components/atoms/Button.svelte";
 </script>
 
 
 
 <h1>Sign in</h1>
 <form method="POST" use:enhance>
-    <button formaction="?/github">Sign in with GitHub</button>
+    <Button type="submit" formaction="?/github">
+        Sign in with GitHub
+    </Button>
     <button formaction="?/google">Sign in with Google</button>
 </form>

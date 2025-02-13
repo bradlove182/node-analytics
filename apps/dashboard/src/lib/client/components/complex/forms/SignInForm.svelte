c4081e131b5178f0c99f59b13769235e5f8e7e34
Bradley Love <brad@bradlove.co.za>
2025-02-13 16:53:42 +0200

    SignInForm.svelte

---
+++ apps/dashboard/src/lib/client/components/complex/forms/SignInForm.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import { enhance } from "$app/forms";
+    import { Button } from "$lib/client/components/base/button";
+    import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "$lib/client/components/base/card";
+
+    let loading = $state<boolean>(false);
+
+    const disabled = $derived(loading);
+</script>
+
+<Card class="min-w-100">
+    <CardHeader>
+        <CardTitle level={1}>Sign In</CardTitle>
+        <CardDescription>Choose a auth provider</CardDescription>
+    </CardHeader>
+    <CardContent>
+        <form method="POST" use:enhance onsubmit={() => loading = true}>
+            <fieldset class="flex flex-col gap-4" {disabled}>
+                <Button type="submit" formaction="/login?/github">
+                    Sign in with GitHub
+                </Button>
+                <Button type="submit" formaction="/login?/google">
+                    Sign in with Google
+                </Button>
+            </fieldset>
+        </form>
+    </CardContent>
+</Card>
---
+++ apps/dashboard/src/lib/client/components/complex/forms/index.ts
@@ -0,0 +1,5 @@
+import SignInForm from "./SignInForm.svelte"
+
+export {
+    SignInForm,
+}
